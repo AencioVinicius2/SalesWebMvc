c0666302bf1b1c171c48c20b9e4ffd69188a3a0e
Aencio <aenpvp9@gmail.com>
2024-08-07 16:47:42 -0300

    fix: ErrorViewModel import fixed

--- SalesWebMvc/SalesWebMvc/Program.cs
+++ SalesWebMvc/SalesWebMvc/Program.cs
@@ -1,3 +1,5 @@
+//using SalesWebMvc.Models.ViewModels;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,8 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment()) {
+if (!app.Environment.IsDevelopment())
+{
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
