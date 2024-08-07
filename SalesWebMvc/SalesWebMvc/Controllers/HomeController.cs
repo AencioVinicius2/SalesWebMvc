6eee1c82a931496e7750c06f0985be7ca3675fd7
Aencio <aenpvp9@gmail.com>
2024-08-07 14:49:06 -0300

    feat: add DepartmentsController

--- SalesWebMvc/SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/SalesWebMvc/Controllers/HomeController.cs
@@ -3,24 +3,30 @@ using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
 using System.Diagnostics;
 
-namespace SalesWebMvc.Controllers {
-    public class HomeController : Controller {
+namespace SalesWebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger) {
+        public HomeController(ILogger<HomeController> logger)
+        {
             _logger = logger;
         }
 
-        public IActionResult Index() {
+        public IActionResult Index()
+        {
             return View();
         }
 
-        public IActionResult Privacy() {
+        public IActionResult Privacy()
+        {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
+        public IActionResult Error()
+        {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
